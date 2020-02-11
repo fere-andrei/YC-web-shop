2934235f8abc297ee24fc39032602ae309d6524e
fere-andrei <andrei095@yahoo.com>
2020-02-11 16:06:37 +0200

    afisare detalii ale order-ului din pagina de order ca popup

---
+++ target/web-app-shop-1.0-SNAPSHOT/js/allJavaScripts.js
@@ -0,0 +1,162 @@
+$(document).ready(function () {
+    MyCartComponent.bindAll();
+});
+
+var MyCartComponent = (function(){
+    var Config = {
+        //add to cart
+        ADD_TO_CART_BUTTON: ".js-add-to-cart-button",
+        NO_OF_ITEMES:".js-quantity-class",
+
+        //update
+        TOTAL_COST:"#js-place-order",
+        UPDATE_CLASS_ITEMS:".js-update-cart-button",
+        PRICE_OF_ITEM:".js-price-of-item",
+        QUANTITY_TO_UPDATE:".js-quantity-to-update",
+        TABLE_CLASS_TO_UPDATE:".js-product-from-cart",
+
+        //place order
+        PLACE_ORDER: "#js-place-order",
+
+        //order details
+        DETAILS_BUTTON: ".js-show-order-details",
+
+        //common
+        MY_CART_VIEW:"#JS-cart-item-count"
+
+    };
+
+    var bindAllComponents = function () {
+         addToCart();
+         updateCart();
+         placeOrder();
+         showOrderDetails();
+    }
+
+    var addToCart = function () {
+        $(Config.ADD_TO_CART_BUTTON).click(function(event){
+            var productId = $(event.target).val();
+            //var quantity = $(Config.NO_OF_ITEMES).val();
+
+            var quantity = $(event.target).closest(".js-product-details").find(".js-quantity-class").val();
+            $.ajax({
+                type:"POST",
+                data: {
+                    quantity : quantity,
+                    productId : productId,
+                    cartComponent : "addProduct"
+                },
+                url : "cart",
+                success : function(rest){
+                    var inputStock=parseInt($(Config.MY_CART_VIEW).text());
+                    var totalItems = inputStock+parseInt(quantity);
+                    $(Config.MY_CART_VIEW).text(totalItems);
+                },
+                error : function(resp){
+                    alert("FAIL");
+                }
+            });
+        });
+    }
+
+
+
+
+    var updateCart = function () {
+        $(Config.UPDATE_CLASS_ITEMS).click(function(event){
+            var productIdFromCart = $(event.target).val();
+            var newQuantity = $(event.target).closest(".js-product-from-cart").find(".js-quantity-to-update").val();
+
+            $.ajax({
+                type:"POST",
+                data: {
+                    newQuantity : newQuantity,
+                    productIdFromCart : productIdFromCart,
+                    cartComponent : "updateProduct"
+                },
+                url : "cart",
+                success : function(){
+                    if(parseInt(newQuantity)==0){
+                        deleteItem();
+                    }else {
+                        updateItem();
+                    }
+                    updateTotalCost();
+                    updateItemCount();
+                },
+                error : function(){
+                    alert("FAIL");
+                }
+            });
+        });
+    }
+
+    var placeOrder = function () {
+        $(Config.PLACE_ORDER).click(function(event){
+            var orderNumber = $(event.target).val();
+
+            $.ajax({
+                type:"POST",
+                data: {
+                    orderNumber : orderNumber,
+                    cartComponent : "orderDetails"
+                },
+                url : "order",
+                success : function(){
+
+                },
+                error : function(){
+                    alert("FAIL");
+                }
+            });
+        });
+
+    }
+
+
+    var showOrderDetails = function () {
+        $(Config.DETAILS_BUTTON).click(function(event){
+            var orderItems = $(event.target).val();
+
+            $.ajax({
+                type:"POST",
+                data: {
+                    orderItems : orderItems,
+                    orderComponent : "orderDetails"
+                },
+                url : "order",
+                success : function(){
+                    window.open("orderDetails.jsp", "_blank", "scrollbars=1,resizable=1,height=300,width=950");
+
+                },
+                error : function(){
+                    alert("FAIL");
+                }
+            });
+        });
+
+    }
+
+
+    //TODO ADD IMPLEMENTATION IN NEXT METHODS
+    function deleteItem() {
+
+    }
+
+    function updateItem() {
+
+    }
+
+    function updateTotalCost() {
+
+    }
+
+    function updateItemCount() {
+
+    }
+
+
+    return {
+        bindAll: bindAllComponents
+    }
+})();
---
+++ target/web-app-shop-1.0-SNAPSHOT/orderDetails.jsp
@@ -0,0 +1,46 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: anfe0908
+  Date: 2/11/2020
+  Time: 1:29 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+
+<div style="background: #E0E0E0; height: 65px; padding: 5px; ">
+    <div style="float: left; " >
+        <h1>YC Shop</h1>
+    </div>
+
+    <img src="img/app-icon.png" style="width: 55px; height: 55px; padding-bottom: inherit">
+</div>
+
+<head>
+    <title>Order Details</title>
+
+
+    <table border="2" cellpadding="5" align="center">
+        <tr>
+            <th>Product Name</th>
+            <th>Price$</th>
+            <th>Number of units</th>
+        </tr>
+        <c:forEach items="${orderDetailsList}" var="orderDetailsItems">
+            <tr class = "js-product-from-cart">
+                <td><c:out value="${orderDetailsItems.productName}" /></td>
+                <td><c:out value="${orderDetailsItems.price}" /></td>
+                <td> <c:out value="${orderDetailsItems.quantity}" /></td>
+            </tr>
+        </c:forEach>
+
+    </table>
+
+    <jsp:include page="_footer.jsp"></jsp:include>
+
+</head>
+<body>
+
+</body>
+</html>
